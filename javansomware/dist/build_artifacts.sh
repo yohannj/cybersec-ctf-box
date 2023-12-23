fb3d73962e62921ec65b9d31d3617f743549a04d
Yohann Jardin <yohann.jardin@telecomnancy.net>
2023-12-23 16:23:15 +0100

    chore(rev): create encoded flag within build_artifacts.sh

--- javansomware/dist/build_artifacts.sh
+++ javansomware/dist/build_artifacts.sh
@@ -4,10 +4,11 @@ BASEDIR=$(dirname $(realpath "$0"))
 
 # Build zip file containing public resources
 cd $BASEDIR/../challenge/
+java Ransom.java  # Create flag.important.hax
 javac Ransom.java -source 11 -target 11
 jar cfm ransom.jar manifest.mf Ransom.class
 zip -q $BASEDIR/artifacts.zip flag.important.hax ransom.jar
-rm Ransom.class Ransom.jar
+rm Ransom.class Ransom.jar flag.important.hax
 
 # Rename zip file to include its sha256
 SHA=$(sha256sum $BASEDIR/artifacts.zip | cut -d" " -f1)
--- ranxorware/dist/build_artifacts.sh
+++ ranxorware/dist/build_artifacts.sh
@@ -4,7 +4,9 @@ BASEDIR=$(dirname $(realpath "$0"))
 
 # Build zip file containing public resources
 cd $BASEDIR/../challenge/
+python ransom.py
 zip -q $BASEDIR/artifacts.zip flag.important.hax ransom.py
+rm flag.important.hax
 
 # Rename zip file to include its sha256
 SHA=$(sha256sum $BASEDIR/artifacts.zip | cut -d" " -f1)
